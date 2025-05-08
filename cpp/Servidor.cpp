e93dba552db86a31e5faaf6cdc1cd9201124a4fb
nico <cosmonicob@gmail.com>
2025-05-08 10:55:45 +0200

    Constructor de la clase Servidor hecho

--- cpp/Servidor.cpp
+++ cpp/Servidor.cpp
@@ -3,7 +3,15 @@
 #include "cabeceras.h"
 
 Servidor::Servidor(char *dS, char *nJ, int i, int mxL, int mxC, int p, char *lG) {
-    this.direccionServidor=dS;
+    strcpy(direccionServidor,ds);
+    strcpy(nombreJuego,nJ);
+    id=i;
+    maxJugadoresConectados=mxL;
+    maxJugadoresEnEspera=mxC;
+    puerto=p;
+    strcpy(localizacionGeografica,lG);
+    strcpy(estado,"INACTIVO");
+
 
 
 }
